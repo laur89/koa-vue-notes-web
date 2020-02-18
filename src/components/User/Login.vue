a7186de688cd84cc214fe21ad582daf915153ec0
Laur Aliste <laur.aliste@williamhill.com>
2020-02-18 01:38:06 +0100

    edit Chart.vue to use updated trading-vue lib
    - link trading-vue version to local;

--- src/components/User/Login.vue
+++ src/components/User/Login.vue
@@ -24,6 +24,7 @@
                     name="signup-form-username"
                     id="signup-form-username"
                     v-model.trim="credentials.username"
+                    autofocus
                     @input="$v.credentials.username.$touch()"
                   />
                   <div
